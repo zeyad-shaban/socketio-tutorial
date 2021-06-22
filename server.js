cbdbe054e5717fc25ba3451eacdcd1335fca2c37
zeyadShapan <zeyadshapan2004@gmail.com>
2021-06-22 18:07:44 +0200

    Intial Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+
+app.set('views', 'Views');
+app.use(express.static('Views'));
+
+
+app.get('/', (req, res) => {
+    return res.render('index.html');
+});
+
+
+io.on('connection', socket => {
+    socket.on('send-message', data => {
+        socket;
+        io.sockets;
+        socket.broadcast;
+    });
+});
+
+
+server.listen(5000);
